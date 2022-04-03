6b5e97d55312fb0ae37047099ce33cd241ebff91
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-02 22:15:28 -0600

    add homepage and movies views and url patterns

---
+++ movies/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+
+from django.http import HttpResponse
+
+def movies(request):
+    return HttpResponse("Hello there")
+
+def home(request):
+    return HttpResponse("Home page")
