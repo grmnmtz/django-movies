d46eb3ab6e7a2bf5b9c9f5cf79bdb77190bb890e
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-02 23:37:16 -0600

    add delete specific movie and add 404 redirect

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-npp*%vgdw9tltpjfvfmm83$8%$(r$=_(gh20ku@jcwz^k8w*&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
