6ae447e9ef95531a063397559be3d266ea099ea5
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-02 22:29:25 -0600

    add basic views and templates for movies

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+data = {
+    'movies': [
+        {
+            'id': 5,
+            'title': 'Jaws',
+            'year': 1996,
+        },
+        {
+             'id': 4,
+            'title': 'Sharknado',
+            'year': 2001
+        },
+        {
+             'id': 3,
+            'title': 'The Sharks',
+            'year': 1999
+        }
+    ]
+}
 
-from django.http import HttpResponse
+template_name = 'movies/movies.html'
 
 def movies(request):
-    return HttpResponse("Hello there")
+    return render(request, template_name, data)
 
 def home(request):
     return HttpResponse("Home page")
